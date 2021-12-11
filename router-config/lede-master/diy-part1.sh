deca4f5825c815b9da46a350d3b0477bab4ef20f
yudiee666 <95980717+yudiee666@users.noreply.github.com>
2021-12-12 03:06:59 +0700

    Update diy-part1.sh

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -16,13 +16,3 @@
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
-# Add luci-app-openclash
-
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openwrt-openclash
-
-pushd package/openwrt-openclash/tools/po2lmo && make && sudo make install 2>/dev/null && popd
-
-
-# Add luci-app-passwall
-
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
