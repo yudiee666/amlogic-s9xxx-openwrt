037b2bc8e46b3cdb73def2295424384f511c33d9
yudiee666 <95980717+yudiee666@users.noreply.github.com>
2021-12-12 03:10:22 +0700

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -52,4 +52,14 @@ svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
 # git apply ../router-config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Other ends -------------------------------
+# Add luci-app-openclash
+
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openwrt-openclash
+
+pushd package/openwrt-openclash/tools/po2lmo && make && sudo make install 2>/dev/null && popd
+# Add luci-app-passwall
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
+
+
 
