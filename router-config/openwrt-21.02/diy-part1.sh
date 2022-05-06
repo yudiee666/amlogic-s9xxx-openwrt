cf1d980532877bfef6b1412ecee23fee3829df4a
yudiee666 <95980717+yudiee666@users.noreply.github.com>
2022-05-07 02:58:19 +0700

    Update diy-part1.sh

--- router-config/openwrt-21.02/diy-part1.sh
+++ router-config/openwrt-21.02/diy-part1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #========================================================================================================================
 # https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt for Amlogic S9xxx STB
+# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
 # Source code repository: https://github.com/openwrt/openwrt / Branch: 21.02
 #========================================================================================================================
@@ -15,4 +15,3 @@
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
-
